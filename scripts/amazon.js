24a1fa191a7a2530fae1b132d82ee2a19132e8c1
#BlueSwirls1985 <hdzmike412@gmail.com>
2025-10-02 21:21:49 -0500

    cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -55,7 +55,7 @@ products.forEach(product => {
     // console.log(html);
 });
 
-//console.log(productsHTML);
+// console.log(productsHTML);
 
 document.querySelector('.js-products-grid')
     .innerHTML = productsHTML;
@@ -80,7 +80,16 @@ document.querySelectorAll('.js-add-to-cart')
                     quantity: 1
                 });
             }
-            console.log(cart);
+
+            let cartQuantity = 0;
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+                .innerHTML = cartQuantity;
+            // console.log(cartQuantity);
+            // console.log(cart);
         });
     });
 
