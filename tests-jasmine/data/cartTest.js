ffcb2ccd106d72082254799f42ac512e793cef56
#BlueSwirls1985 <hdzmike412@gmail.com>
2025-10-09 22:44:29 -0500

    error -1 was fixed, however is temporary(create a localStorage mock)

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -6,11 +6,22 @@ describe('addToCart', () => {
     });
     //flaky test need to use mock test
     it('should add a new product to cart', () => {
-        spyOn(localStorage, 'getItem').and.callFake(() => {
-            return JSON.stringify([]);
+        // spyOn(localStorage, 'getItem').and.callFake(() => {
+        //     return JSON.stringify([]);
+        // });
+        // spyOn(localStorage, 'setItem');
+        // console.log(localStorage.getItem('cart'));
+
+        // Create a fake localStorage mock
+        const mockLocalStorage = {
+            getItem: jasmine.createSpy('getItem').and.returnValue(JSON.stringify([])),
+            setItem: jasmine.createSpy('setItem')
+        };
+
+        // Replace window.localStorage temporarily
+        Object.defineProperty(window, 'localStorage', {
+            value: mockLocalStorage
         });
-        spyOn(localStorage, 'setItem');
-        console.log(localStorage.getItem('cart'));
         loadFromStorage();
 
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
